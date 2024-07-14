5cd1d37a825530e38de131883e80fe04aecbfe4a
ChrisFuchs12 <chrisfuchs1@icloud.com>
2024-07-14 13:14:30 +0100

    works yaayaya

--- Assets/Scripts/CopyAnim.cs
+++ Assets/Scripts/CopyAnim.cs
@@ -2,19 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CopyAnim : MonoBehaviour
+public class copyAnim : MonoBehaviour
 {
-    
     public Transform target;
-    public ConfigurableJoint joint;
+    private ConfigurableJoint joint;
 
     private Quaternion startingRotation;
     void Start()
     {
+        joint = GetComponent<ConfigurableJoint>();
         startingRotation = transform.rotation;
     }
 
-    
+
     void Update()
     {
         joint.SetTargetRotationLocal(target.rotation, startingRotation);
