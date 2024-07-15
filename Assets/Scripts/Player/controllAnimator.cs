eea997cec27d3552d6ff01941da29176c38afbc6
ChrisFuchs12 <chrisfuchs1@icloud.com>
2024-07-15 21:44:26 +0100

    arms n stuff

--- Assets/Scripts/Player/controllAnimator.cs
+++ Assets/Scripts/Player/controllAnimator.cs
@@ -19,5 +19,17 @@ public class controllAnimator : MonoBehaviour
         }if(Input.GetKeyUp("w")){
             animator.SetBool("Walking", false);
         }
+
+        if(Input.GetMouseButtonDown(0)){
+            animator.SetBool("RightHand", true);
+        }if(Input.GetMouseButtonUp(0)){
+            animator.SetBool("RightHand", false);
+        }
+
+        if(Input.GetMouseButtonDown(1)){
+            animator.SetBool("LeftHand", true);
+        }if(Input.GetMouseButtonUp(1)){
+            animator.SetBool("LeftHand", false);
+        }
     }
 }
